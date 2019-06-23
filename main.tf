bda58189b2d25fe6aeed9f4759e21f8ffa30f9e0
Simon Daniel <12603666+sgdan@users.noreply.github.com>
2019-06-23 14:12:57 +1000

    Add MySQL test with EBS volume on ECS cluster

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 module "simple-vpc" {
   #source = "../tf-modules/simple-vpc"
-  source = "github.com/sgdan/tf-modules//simple-vpc?ref=0.1.0"
+  source = "github.com/sgdan/tf-modules//simple-vpc?ref=0.2.0"
 }
 
 # Test instance, can use SSM Session Manager to log in
@@ -28,3 +28,14 @@ module "ssm-instance" {
 #   source          = "../tf-modules/eks-config"
 #   worker_role_arn = module.eks-workers.worker_role_arn
 # }
+
+module "ecs" {
+  #source = "../tf-modules/ecs"
+  source = "github.com/sgdan/tf-modules//ecs?ref=0.2.0"
+  vpc_id = module.simple-vpc.vpc_id
+  subnet_ids = module.simple-vpc.private_subnet_ids
+}
+module "ecs-mysql" {
+  #source = "../tf-modules/ecs-mysql"
+  source = "github.com/sgdan/tf-modules//ecs-mysql?ref=0.2.0"
+}
